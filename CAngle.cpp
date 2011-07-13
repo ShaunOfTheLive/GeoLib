68c1b68f2c5309329358017d26eaa435ff2b1536
ShaunOfTheLive <ShaunOfTheLive@f751c796-209d-45e0-abb8-28d692f85dff>
2011-07-13 05:04:15 +0000

    - switched to per_unit_data - switched from convertToDeg/Rad to convert()

--- CAngle.cpp
+++ CAngle.cpp
@@ -13,20 +13,20 @@ double Angle::fmod(double x, double y) {
   return x - y * floor(x / y);
 }
 
-double convertToDeg(double radians) {
-  double degrees = radians * 180 / M_PI;
-  return degrees;
-}
-
-double convertToRad(double degrees) {
-  double radians = degrees * M_PI / 180;
-  return radians;
+double convert(Unit from_unit, Unit to_unit, double angle)
+{
+  double converted = angle * per_unit_data[to_unit].circle / per_unit_data[from_unit].circle;
+  return converted;
 }
 
 Angle::Angle(Unit unit, double angle)
-: unit(unit), rollover_d(360), rollover_r(2*M_PI)
+: unit(unit),
+  per_unit_data[Degrees].range_min = 0,   per_unit_data[Radians].range_min = 0,
+  per_unit_data[Degrees].range_max = 360, per_unit_data[Radians].range_max = 2*M_PI,
+  per_unit_data[Degrees].circle =    360, per_unit_data[Radians].circle    = 2*M_PI
 {
   set(unit, angle);
+  data = &per_unit_data[unit];
 }
 
 double Angle::getRadians() const
@@ -58,49 +58,56 @@ double Angle::get(Unit unit) const
 void Angle::setRadians(double radians)
 {
   angle_r = radians;
-  angle_d = convertToDeg(radians);
+  angle_d = convert(Radians, Degrees, radians);
 }
 
 void Angle::setDegrees(double degrees)
 {
   angle_d = degrees;
-  angle_r = convertToRad(degrees);
+  angle_r = convert(Degrees, Radians, degrees);
 }
 
+/* set(Unit, angle) sets the unit and then calls set(double) */
 void Angle::set(Unit unit, double angle)
 {
-  switch(unit) {
-  case Radians:
-    angle_r = angle;
-    angle_d = convertToDeg(angle);
-    break;
-  case Degrees:
-    angle_d = angle;
-    angle_r = convertToRad(angle);
-    break;
-  default:
-    throw "Unit must be Radians or Degrees";
-  }
+  setUnit(unit);
+  set(angle);
 }
 
-double get() const
+double Angle::get() const
 {
   return get(getUnit());
 }
 
-void set(double angle)
+/* set(double) directly sets the angle using the current unit */
+void Angle::set(double angle)
 {
-  set(getUnit(), angle);
+  data->angle = angle;
 }
 
-Unit getUnit() const
+Unit Angle::getUnit() const
 {
   return this->unit;
 }
 
-void setUnit(Unit unit)
+void Angle::setUnit(Unit unit)
 {
   this->unit = unit;
+  data = &per_unit_data[unit];
+}
+
+vector<double> Angle::getRange() const
+{
+  vector<double> range;
+  range.push_back(data->range_min);
+  range.push_back(data->range_max);
+  return range;
+}
+
+void Angle::setRange(double min, double max)
+{
+  data->range_min = min;
+  data->range_max = max;
 }
 
 double Angle::cos() const
@@ -116,14 +123,14 @@ double Angle::sin() const
 Angle Angle::operator+=(const Angle &rhs)
 {
   set(get() + rhs.get());  //angle_r += rhs.angle_r;
-  set(fmod(get(), getUnit() == Degrees? rollover_d: rollover_r)); // angle_r = fmod(angle_r,2*M_PI);
+  set(fmod(get(), data->range_max)); // angle_r = fmod(angle_r,2*M_PI);
   return *this;
 }
 
 Angle Angle::operator-=(const Angle &rhs)
 {
   set(get() - rhs.get());  //angle_r -= rhs.angle_r;
-  set(fmod(get(), getUnit() == Degrees? rollover_d: rollover_r)); // angle_r = fmod(angle_r,2*M_PI);
+  set(fmod(get(), data->range_max)); // angle_r = fmod(angle_r,2*M_PI);
   return *this;
 }
 
--- CAngle.h
+++ CAngle.h
@@ -7,25 +7,36 @@ class Angle
   double angle_r;
   double rollover_d;
   double rollover_r;
+  struct per_unit_data
+  {
+    double angle;
+    double range_min;
+    double range_max;
+    double circle;
+  }
+  per_unit_data* data;
   Unit unit;
   static double fmod(double x, double y);
-  static double convertToDeg(double radians);
-  static double convertToRad(double degrees);
+  static double convert(Unit from_unit, Unit to_unit, double angle);
 public:
   Angle(Unit unit, double angle);
 
   enum Unit{Radians, Degrees};
 
+/* deprecated */
   double getRadians() const;
   void setRadians(double radians);
   double getDegrees() const;
   void setDegrees(double degrees);
+/* */
   double get(Unit unit) const;
   void set(Unit unit, double angle);
   double get() const;
   void set(double angle);
   Unit getUnit() const;
   void setUnit(Unit unit);
+  vector<double> getRange() const;
+  void setRange(double min, double max);
   double cos() const;
   double sin() const;
 
