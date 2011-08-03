7449b0f97d4538b4c64f365a87bef0a61ef38345
ShaunOfTheLive <ShaunOfTheLive@f751c796-209d-45e0-abb8-28d692f85dff>
2011-08-03 22:28:04 +0000

    - deleted old testAngle.cpp - moved unit++.cpp to testAngle.cpp

--- testAngle.cpp
+++ testAngle.cpp
@@ -1,65 +1,78 @@
-int testAngle()
-{
-  bool passed = true;
-  double deg1 = 359.0;
-  double deg2 = 2.0;
-  double rad1 = 1.9*M_PI;
-  double rad2 = 0.2*M_PI;
-  Angle a1 = Angle(Angle::Degrees, deg1);
-  Angle a2 = Angle(Angle::Degrees, deg2);
-  Angle a1_p_a2 = a1 + a2;
+#include <iostream>
+#include <string>
+#include <unit++/unit++.h>
+#include "CAngle.h"
+#include "math.h"
 
-  cout << "testAngle: " << deg1 << " deg + " << deg2 << " deg = " << a1_p_a2.get(Angle::Degrees) << endl;
+using namespace std;
+using namespace unitpp;
 
-  if (passed) {
-    // check that a1 + a2 in degrees is equal to the degree wraparound of addition of deg1 and deg2
-    passed = feq(a1_p_a2.get(Angle::Degrees), deg1 + deg2 - 360);
-  }
+namespace {
+  class CAngleTest: public suite
+  {
+    double deg1, deg2, deg3;
+    double rad1, rad2, rad3;
+    Angle adeg1, adeg2, arad1, arad2;
 
-  Angle a2_pe_a1 = Angle(Angle::Degrees, deg1);
-  a2_pe_a1 += Angle(Angle::Degrees, deg2);
+    void test_constructors()
+    {
+      Angle atestdeg(Angle::Degrees, deg1);
+      Angle atestrad(Angle::Radians, rad1);
 
-  cout << "testAngle: " << deg2 << " deg += " << deg1 << " deg: " << a2_pe_a1.get(Angle::Degrees) << endl;
-  if (passed) {
-    // check that the angle of a2 += a1 is equal to the degree wraparound of addition of deg1 and deg2
-    passed = feq(a2_pe_a1.get(Angle::Degrees), deg1 + deg2 - 360);
-  }
+      assert_true("construct angle with degrees", feq(atestdeg.get(), deg1));
+      assert_true("construct angle with radians", feq(atestrad.get(), rad1));
+    }
+    void test_getset()
+    {
+      Angle atestdeg(Angle::Degrees, deg1);
+      Angle atestrad(Angle::Radians, rad1);
+      atestdeg.set(deg3);
+      atestrad.set(rad3);
 
-  cout << "testAngle: " << deg1 << " deg + " << deg2 << " deg to rad = " << a1_p_a2.get(Angle::Radians) << endl;
-  if (passed) {
-    // check that the radian angle of a1_p_a2 is equal to the proper radian amount
-    passed = feq(a1_p_a2.get(Angle::Radians), M_PI / 180 * a1_p_a2.get(Angle::Degrees));
-  }
+      assert_true("set and get with degrees", feq(atestdeg.get(), deg3));
+      assert_true("set and get with radians", feq(atestrad.get(), rad3));
+    }
+    void test_getset_units()
+    {
+      Angle atestdeg(Angle::Degrees, deg1);
+      Angle atestrad(Angle::Radians, rad1);
+      atestdeg.set(Angle::Degrees, deg3);
+      atestrad.set(Angle::Radians, rad3);
 
-  Angle a4 = Angle(Angle::Radians, rad1);
-  Angle a5 = Angle(Angle::Radians, rad2);
-  Angle a4_p_a5 = a4 + a5;
+      assert_true("set and get with degrees", feq(atestdeg.get(Angle::Degrees), deg3));
+      assert_true("set and get with radians", feq(atestrad.get(Angle::Radians), rad3));
+    }
+    void test_operator_plus()
+    {
+      Angle adegadd = adeg1 + adeg2;
+      Angle aradadd = arad1 + arad2;
 
-  cout << "testAngle: " << rad1 << " rad + " << rad2 << " rad = " << a4_p_a5.get(Angle::Radians) << endl;
-  if (passed) {
-    // check that a4 + a5 in radians is equal to the radian wraparound of addition of rad1 and rad2
-    passed = feq(a4_p_a5.get(Angle::Radians), rad1 + rad2 - 2*M_PI);
-  }
+      assert_true("operator+ with degrees", feq(adegadd.get(Angle::Degrees), deg1 + deg2 - 360));
+      assert_true("operator+ with radians", feq(aradadd.get(Angle::Radians), rad1 + rad2 - 2*M_PI));
+    }
+    void test_operator_plus_eq()
+    {
+      Angle adegaddeq = adeg2;
+      adegaddeq += adeg1;
+      Angle aradaddeq = arad2;
+      aradaddeq += arad1;
 
-  Angle a5_pe_a4 = Angle(Angle::Radians, 1.9*M_PI);
-  a5_pe_a4+= Angle(Angle::Radians, 0.2*M_PI);
-  cout << "testAngle: " << rad2 << " rad += " << rad1 << " rad = " << a5_pe_a4.get(Angle::Radians) << endl;
-  if (passed) {
-    // check that a5 += a4 in radians is equal to the radian wraparound of addition of rad1 and rad2
-    passed = feq(a5_pe_a4.get(Angle::Radians), rad1 + rad2 - 2*M_PI);
-  }
-
-  cout << "testAngle: " << rad1 << " rad + " << rad2 << " rad to deg = " << a4_p_a5.get(Angle::Degrees) << endl;
-  if (passed) {
-    // check that the degree angle of a4_p_a5 is equal to the proper degree amount
-    passed = feq(a4_p_a5.get(Angle::Degrees), 180 / M_PI * a4_p_a5.get(Angle::Radians));
-  }
-
-
-  if (passed) {
-    return TEST_PASSED;
-  } else {
-    return TEST_FAILED;
-  }
-//  return TEST_UNDEFINED;
+      assert_true("operator+= with degrees", feq(adegaddeq.get(Angle::Degrees), deg1 + deg2 - 360));
+      assert_true("operator+= with radians", feq(aradaddeq.get(Angle::Radians), rad1 + rad2 - 2*M_PI));
+    }
+  public:
+    CAngleTest() : suite("CAngle tests"),
+    deg1(359.0), deg2(2.0), deg3(50.0),
+    rad1(1.9*M_PI), rad2(0.2*M_PI), rad3(0.5*M_PI),
+    adeg1(Angle::Degrees, deg1), adeg2(Angle::Degrees, deg2), arad1(Angle::Radians, rad1), arad2(Angle::Radians, rad2)
+    {
+      add("test_constructors", testcase(this, "Test constructors", &CAngleTest::test_constructors));
+      add("test_getset", testcase(this, "Test getters and setters", &CAngleTest::test_getset));
+      add("test_getset_units", testcase(this, "Test getters and setters specifying native units", &CAngleTest::test_getset_units));
+      add("test_operator_plus", testcase(this, "Test operator+", &CAngleTest::test_operator_plus));
+      add("test_operator_plus_eq", testcase(this, "Test operator+=", &CAngleTest::test_operator_plus_eq));
+      suite::main().add("CAngleTestSuite", this);
+    }
+  };
+  CAngleTest* CAngleTestSuite = new CAngleTest();
 }
